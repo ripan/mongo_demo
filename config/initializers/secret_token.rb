597597c75975a10af6a3f978caf4dda4cdb32180
ripan <er.ripankumar@gmail.com>
2013-09-30 11:15:51 +0100

    Set rails app with mongo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoDemo::Application.config.secret_token = '06ed98f47a4ce9ebded74d46e1a50581666bbeadda96fee3f7076ee81f6ef3be029616c77fab83d0adfd2e81e552c155f261346f7fbcfb84032b00a72eaf4ba2'
